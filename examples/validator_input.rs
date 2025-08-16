11621625d9f0aaf8b292d3237e89c14a1820a02d
ohmycloud <ohmycloudy@gmail.com>
2025-08-16 16:07:38 +0800

    propagate validation errors

--- examples/validator_input.rs
+++ examples/validator_input.rs
@@ -1,4 +1,6 @@
-use actix_web::{App, Error, HttpResponse, HttpServer, post, web};
+#![feature(str_as_str)]
+
+use actix_web::{App, HttpResponse, HttpServer, post, web};
 use actix_web_validator::Json;
 use serde::Deserialize;
 use validator::{Validate, ValidationError};
@@ -26,10 +28,32 @@ fn validate_password_strength(password: &str) -> Result<(), ValidationError> {
     Ok(())
 }
 
+async fn validate_data<T: Validate>(data: &T) -> Result<(), actix_web::Error> {
+    if let Err(errors) = data.validate() {
+        let mut error_details = Vec::new();
+        for (field, errors) in errors.field_errors() {
+            let messages: Vec<&str> = errors
+                .iter()
+                .map(|e| {
+                    let m = e.message.as_ref().unwrap();
+                    m.as_str()
+                })
+                .collect();
+            error_details.push(format!("{}: {}", field, messages.join(", ")));
+        }
+        let error_message = format!("Validation errors: {}", error_details.join("; "));
+        return Err(actix_web::error::ErrorBadRequest(error_message));
+    }
+    Ok(())
+}
+
 #[post("/register")]
-async fn register(user: Json<NewUser>) -> Result<HttpResponse, Error> {
+async fn register(user: Json<NewUser>) -> Result<HttpResponse, actix_web::Error> {
+    let user = user.into_inner();
+    validate_data(&user).await?;
+
     // Save valid user data
-    Ok(HttpResponse::Created().body(format!("User: {:?} created", user.into_inner())))
+    Ok(HttpResponse::Created().body(format!("User: {:?} created", user)))
 }
 
 #[actix_web::main]
